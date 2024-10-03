317fc134261961b255878ce63b7d7f1e42625e65
Aadityavv <aadityavv9@gmail.com>
2024-10-04 00:17:54 +0530

    new files added

--- static/script.js
+++ static/script.js
@@ -97,3 +97,9 @@ document.getElementById("recordBtn").addEventListener("click", function() {
         isRecording = false;
     }
 });
+
+const blurCircle = document.querySelector("#bgcolor");
+document.addEventListener("mousemove", (e) => {
+    blurCircle.style.left = `${e.clientX - blurCircle.offsetWidth / 2}px`;
+    blurCircle.style.top = `${e.clientY - blurCircle.offsetHeight / 2}px`;
+});
